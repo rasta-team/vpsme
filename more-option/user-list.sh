ac80b95c4fb511c005664872431d1ab0daeab2d8
irawancandra6699 <134005006+irawancandra6699@users.noreply.github.com>
2023-06-30 18:53:26 +0700

    Update user-list.sh

--- more-option/user-list.sh
+++ more-option/user-list.sh
@@ -1,6 +1,6 @@
 # Created by NevermoreSSH
 #wget https://github.com/${GitUser}/
-GitUser="NevermoreSSH"
+GitUser="irawancandra6699"
 #IZIN SCRIPT
 MYIP=$(curl -sS ipv4.icanhazip.com)
 echo -e "\e[32mloading...\e[0m"
