1f46f151dae0449260deb65c597445c012dcd025
irawancandra6699 <134005006+irawancandra6699@users.noreply.github.com>
2023-06-30 18:36:12 +0700

    Update set-br.sh

--- install/set-br.sh
+++ install/set-br.sh
@@ -2,19 +2,19 @@
 GitUser="irawancandra6699"
 curl https://rclone.org/install.sh | bash
 printf "q\n" | rclone config
-wget -O /root/.config/rclone/rclone.conf "https://raw.githubusercontent.com/${GitUser}/sapphire/main/rclone.conf"
+wget -O /root/.config/rclone/rclone.conf "https://raw.githubusercontent.com/${GitUser}/vpsme/main/rclone.conf"
 git clone  https://github.com/${GitUser}/wondershaper.git
 cd wondershaper
 make install
 cd
 rm -rf wondershaper
 cd /usr/bin
-wget -O autobackup "https://raw.githubusercontent.com/${GitUser}/sapphire/main/system/backupBot.sh"
-wget -O backup "https://raw.githubusercontent.com/${GitUser}/sapphire/main/system/backup.sh"
-wget -O bckp "https://raw.githubusercontent.com/${GitUser}/sapphire/main/system/bckp.sh"
-wget -O restore "https://raw.githubusercontent.com/${GitUser}/sapphire/main/system/restore.sh"
-wget -O strt "https://raw.githubusercontent.com/${GitUser}/sapphire/main/system/strt.sh"
-wget -O limit-speed "https://raw.githubusercontent.com/${GitUser}/sapphire/main/limit-speed.sh"
+wget -O autobackup "https://raw.githubusercontent.com/${GitUser}/vpsme/main/system/backupBot.sh"
+wget -O backup "https://raw.githubusercontent.com/${GitUser}/vpsme/main/system/backup.sh"
+wget -O bckp "https://raw.githubusercontent.com/${GitUser}/vpsme/main/system/bckp.sh"
+wget -O restore "https://raw.githubusercontent.com/${GitUser}/vpsme/main/system/restore.sh"
+wget -O strt "https://raw.githubusercontent.com/${GitUser}/vpsme/main/system/strt.sh"
+wget -O limit-speed "https://raw.githubusercontent.com/${GitUser}/vpsme/main/limit-speed.sh"
 chmod +x autobackup
 chmod +x backup
 chmod +x bckp
